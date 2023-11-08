6dbc9c009e2608df7df345155d25877c0bb7fb3e
Kateryna2882 <111122172+Kateryna2882@users.noreply.github.com>
2023-11-08 19:53:44 +0200

    Add new fields in CoffeeOrderBoard class

--- src/CoffeeOrderBoard.java
+++ src/CoffeeOrderBoard.java
@@ -1,37 +1,41 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class CoffeeOrderBoard {
-    private List<Order>orders = new ArrayList<>();
-    private int nextOrderNumber=1;
-    public void add(Order order){
-        order.setOrderNumber(nextOrderNumber);
-        orders.add(order);
-        nextOrderNumber++;
-    }
-    public void deliver(){
-        if (!orders.isEmpty()){
-            Order nextOrder = orders.get(0);
-            System.out.println("Deliver ing order : " + nextOrder.getOrderNumber() + " | "
-                    + nextOrder.getCustomerName());
-            orders.remove(0);
-        } else {
-            System.out.println("No orders to deliver");
-        }
-    }
-    public void deliver(int orderNumber){
-        for (int i = 0; i < orders.size(); i++) {
-            Order order = orders.get(i);
-            if (order.getOrderNumber() == orderNumber){
-                System.out.println("Deliver ing order " + order.getOrderNumber() + " | " + order.getCustomerName());
-                orders.remove(i);
-                return;
-            }
+    private static int totalOrder = 1;
+    private final Queue<Order> orders;
 
-        }
-        System.out.println( "Order with number " + orderNumber + " not found. ");
+    public CoffeeOrderBoard() {
+        this.orders = new LinkedList <>();
     }
-    public void draw (){
+
+    public void add(String customerName) {
+        Order newOrder = new Order(totalOrder, customerName);
+        orders.add(newOrder);
+        totalOrder++;
+    }
+
+    public Order deliver() {
+        return orders.poll();
+    }
+
+    public Order deliver(int number) {
+        if (orders.isEmpty()) {
+            return null;
+        }
+        if (orders.element().getOrderNumber() == number) {
+            return orders.poll();
+        }
+        for (Order order : orders)
+            if (order.getOrderNumber() == number) {
+                orders.remove(order);
+                return order;
+            }
+        return null;
+//    }
+//    return null;
+}
+    public void draw() {
         System.out.println("*********************************************************************************");
         System.out.println("Num | Name ");
         for (Order order : orders) {
